8cae61c0ed950674a896617f4dd0458276f020da
DevMind <DevMind@DESKTOP-O8KJUGO>
2023-11-08 13:01:35 +0100

    test

--- src/Calculator.java
+++ src/Calculator.java
@@ -4,6 +4,6 @@ public class Calculator {
 	public static void main(String[] args) {
 		FrameCalculator frame = new FrameCalculator();
 		frame.setVisible(true);
-		frame.setDefaultCloseOperation(3); // == JFrame.EXIT_ON_CLOSE
+		frame.setDefaultCloseOperation(3);
 	}
 }
